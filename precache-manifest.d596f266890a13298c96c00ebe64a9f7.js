8cfd2a19c16ebfa6eeccd25ff729074796ea6be5
Dominic Martindale <awalletsdream@gmail.com>
2020-12-03 13:06:27 +1300

    Updates

---
+++ precache-manifest.d596f266890a13298c96c00ebe64a9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91724cbee46121876e05bf98590e6431",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37f036a30169da40da3e",
+    "url": "./static/css/main.12edc5eb.chunk.css"
+  },
+  {
+    "revision": "65594d7fb19491b618ad",
+    "url": "./static/js/2.b9f5d00c.chunk.js"
+  },
+  {
+    "revision": "273c4ef3243037f5827d8f065b38b974",
+    "url": "./static/js/2.b9f5d00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f036a30169da40da3e",
+    "url": "./static/js/main.400286fa.chunk.js"
+  },
+  {
+    "revision": "9f0f00dc7d80d7fb589b",
+    "url": "./static/js/runtime-main.e3fa9d86.js"
+  }
+]);
